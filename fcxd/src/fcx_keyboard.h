f93591510b623a90b0be004b16829b472eadde5b
Francesco Burelli <francesco.burelli@protonmail.com>
2022-10-27 01:57:41 +0200

    fcxd linux keyboard_type_keycode exposed

--- fcxd/src/fcx_keyboard.h
+++ fcxd/src/fcx_keyboard.h
@@ -5,3 +5,5 @@ fcx_keyboard_t *fcx_keyboard_create(const char *keymap_name);
 void fcx_keyboard_free(fcx_keyboard_t *keyboard);
 
 int fcx_keyboard_type_text(fcx_keyboard_t *keyboard, const char *text);
+
+int fcx_keyboard_type_keycode(fcx_keyboard_t *keyboard, int keycode);
