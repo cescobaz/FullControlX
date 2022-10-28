2fd079d3106c4c65b48b9bf08b396500181bf9fd
Francesco Burelli <francesco.burelli@gmail.com>
2022-10-28 18:31:07 +0200

    first working impl of mac keyboard

--- fcxd/src/fcx_keyboard.h
+++ fcxd/src/fcx_keyboard.h
@@ -6,6 +6,4 @@ void fcx_keyboard_free(fcx_keyboard_t *keyboard);
 
 int fcx_keyboard_type_text(fcx_keyboard_t *keyboard, const char *text);
 
-int fcx_keyboard_type_keycode(fcx_keyboard_t *keyboard, int keycode);
-
 int fcx_keyboard_type_symbol(fcx_keyboard_t *keyboard, const char *symbol);
--- fcxd/src/mac/fcx_keyboard.c
+++ fcxd/src/mac/fcx_keyboard.c
@@ -6,8 +6,41 @@
 #include <IOKit/hidsystem/IOHIDLib.h>
 #include <IOKit/hidsystem/ev_keymap.h>
 #include <MacTypes.h>
+#include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/fcntl.h>
 
-struct fcx_keyboard {};
+#define UNICODE_STRING_MAX_LEN 3
+
+// The Carbon key modifiers parameter is a UInt32, not a UInt16. But
+// EventModifiers is UInt16. However activeFlag and btnState make no
+// sense and they use the first 8 bits. So I shift of 8 bits.
+static UInt32 shift = shiftKey >> 8;                             // 0x0002;
+static UInt32 option = optionKey >> 8;                           // 0x0008;
+static UInt32 option_shift = (shiftKey >> 8) | (optionKey >> 8); // 0x000A;
+
+struct fcx_keyboard_keycode_mapping {
+  // The encoding seems to be NSUTF16LittleEndianStringEncoding, indeed the
+  // following works:
+  /*
+   * [[NSString alloc] initWithBytes:unicode_string
+   *                   length:unicode_string_len * sizeof(UniChar)
+   *                   encoding:NSUTF16LittleEndianStringEncoding];
+   */
+  UniChar unicode_string[UNICODE_STRING_MAX_LEN];
+
+  UniCharCount unicode_string_len;
+  UInt32 modifiers;
+  UInt16 keycode;
+};
+
+struct fcx_keyboard {
+  TISInputSourceRef keyboard_layout_ref;
+  struct fcx_keyboard_keycode_mapping *keycode_mapping;
+  UInt32 keycode_mapping_len;
+};
 
 fcx_keyboard_t *fcx_keyboard_create(const char *keymap_name) {
   UInt32 keyboardType = LMGetKbdType();
@@ -23,46 +56,95 @@ fcx_keyboard_t *fcx_keyboard_create(const char *keymap_name) {
     FCX_LOG_ERR("keyboardLayoutRef == NULL");
     return NULL;
   }
+  struct fcx_keyboard *keyboard = malloc(sizeof(struct fcx_keyboard));
+  keyboard->keyboard_layout_ref = keyboardLayoutRef;
 
   CFDataRef keyboardLayoutDataRef = (CFDataRef)TISGetInputSourceProperty(
       keyboardLayoutRef, kTISPropertyUnicodeKeyLayoutData);
   const UCKeyboardLayout *keyboardLayout =
       (const UCKeyboardLayout *)CFDataGetBytePtr(keyboardLayoutDataRef);
-  EventModifiers modifiers[] = {0, shiftKey, optionKey, shiftKey | optionKey};
-  UniCharCount unicodeStringMaxLen = 4;
-  UniChar unicodeString[5];
+  UInt32 keycode_index = 0;
+  UInt32 keycode_len = 0;
+  UniCharCount unicodeStringMaxLen = UNICODE_STRING_MAX_LEN;
+  UniChar unicodeString[UNICODE_STRING_MAX_LEN];
   UniCharCount unicodeStringLen;
-  for (int i = 0; i < 4; i++) {
-    EventModifiers modifier = modifiers[i];
+  UInt32 modifiers[] = {0, shift, option, option_shift};
+  UInt16 modifiers_len = 4;
+  for (int op = 0; op < 2; op++) {
+    if (op == 1) {
+      keyboard->keycode_mapping =
+          malloc(sizeof(struct fcx_keyboard_keycode_mapping) * keycode_len);
+      assert(keyboard->keycode_mapping != NULL);
+      keyboard->keycode_mapping_len = keycode_len;
+    }
+    for (UInt16 i = 0; i < modifiers_len; i++) {
+      UInt32 modifier = modifiers[i];
 
-    for (int keycode = 1; keycode < NX_NUMKEYCODES; keycode++) {
-      UInt32 deadKeyState = 0;
-      unicodeStringLen = 0;
-      OSStatus rc = UCKeyTranslate(
-          keyboardLayout, keycode, kUCKeyActionDown, ((modifier >> 8) & 0xFFFF),
-          keyboardType, kUCKeyTranslateNoDeadKeysMask, &deadKeyState,
-          unicodeStringMaxLen, &unicodeStringLen, unicodeString);
-      unicodeString[unicodeStringLen] = 0;
-      if (rc != 0 || unicodeStringLen != 0 || deadKeyState != 0) {
-        UniChar c = unicodeString[0];
-        if (c < 32 || c > 255) {
-          c = 0;
+      for (int keycode = 0; keycode < NX_NUMKEYCODES; keycode++) {
+        UInt32 deadKeyState = 0;
+        unicodeStringLen = 0;
+        OSStatus rc = UCKeyTranslate(
+            keyboardLayout, keycode, kUCKeyActionDown, modifier, keyboardType,
+            kUCKeyTranslateNoDeadKeysMask, &deadKeyState, unicodeStringMaxLen,
+            &unicodeStringLen, unicodeString);
+        if (rc != 0) {
+          FCX_LOG_DEBUG("UCKeyTranslate fails, rc: %d", rc);
+          continue;
         }
-        FCX_LOG_DEBUG(
-            "UCKeyTranslate %d: modifier %08X, keycode %d, deadKeyState %d, "
-            "str len %d, -- %C -- %X",
-            rc, (UInt32)modifier, keycode, deadKeyState, unicodeStringLen, c,
-            unicodeString[0]);
+        if (unicodeStringLen == 0) {
+          continue;
+        }
+        if (deadKeyState != 0) {
+          FCX_LOG_DEBUG("UCKeyTranslate deadKeyState: %d", deadKeyState);
+          continue;
+        }
+        if (op == 0) {
+          keycode_len += 1;
+        } else {
+          struct fcx_keyboard_keycode_mapping keycode_mapping;
+          keycode_mapping.keycode = keycode;
+          keycode_mapping.modifiers = modifier;
+          keycode_mapping.unicode_string_len = unicodeStringLen;
+          memset(keycode_mapping.unicode_string, UNICODE_STRING_MAX_LEN,
+                 sizeof(UniChar));
+          memcpy(keycode_mapping.unicode_string, unicodeString,
+                 unicodeStringLen * sizeof(UniChar));
+          keyboard->keycode_mapping[keycode_index] = keycode_mapping;
+          keycode_index += 1;
+        }
       }
     }
   }
 
-  return 0;
+  return keyboard;
 }
-void fcx_keyboard_free(fcx_keyboard_t *keyboard) {}
+void fcx_keyboard_free(fcx_keyboard_t *keyboard) {
+  struct fcx_keyboard *kb = (struct fcx_keyboard *)keyboard;
+  free(kb->keycode_mapping);
+  free(kb);
+}
 
-int fcx_keyboard_type_text(fcx_keyboard_t *keyboard, const char *text) {
-  return 1;
+struct fcx_keyboard_keycode_mapping *
+_fcx_keyboard_keycode_mapping_from_char(fcx_keyboard_t *keyboard, char c) {
+  struct fcx_keyboard *kb = (struct fcx_keyboard *)keyboard;
+  for (int mi = 0; mi < kb->keycode_mapping_len; mi++) {
+    struct fcx_keyboard_keycode_mapping km = kb->keycode_mapping[mi];
+    if (km.unicode_string[0] == c) {
+      return &(kb->keycode_mapping[mi]);
+    }
+  }
+  FCX_LOG_WARN("_fcx_keyboard_keycode_mapping_from_char not found char %c", c);
+  return NULL;
+}
+
+int fcx_keyboard_set_modifiers(fcx_keyboard_t *keyboard,
+                               UInt32 modifiers_mask) {
+  IOGPoint loc = {0, 0};
+  NXEventData event;
+  memset(&event, 0, sizeof(NXEventData));
+  return IOHIDPostEvent(fcx_io_hid_connect(), NX_FLAGSCHANGED, loc, &event,
+                        kNXEventDataVersion, modifiers_mask,
+                        kIOHIDSetGlobalEventFlags);
 }
 
 int fcx_keyboard_set_keycode_state(fcx_keyboard_t *keyboard, int keycode,
@@ -72,13 +154,37 @@ int fcx_keyboard_set_keycode_state(fcx_keyboard_t *keyboard, int keycode,
   memset(&event, 0, sizeof(NXEventData));
   event.key.keyCode = keycode;
   return IOHIDPostEvent(fcx_io_hid_connect(), state, loc, &event,
-                        kNXEventDataVersion, kIOHIDSetGlobalEventFlags,
-                        kIOHIDPostHIDManagerEvent);
+                        kNXEventDataVersion, 0, kIOHIDPostHIDManagerEvent);
 }
 
 int fcx_keyboard_type_keycode(fcx_keyboard_t *keyboard, int keycode) {
   fcx_keyboard_set_keycode_state(keyboard, keycode, NX_KEYDOWN);
   fcx_keyboard_set_keycode_state(keyboard, keycode, NX_KEYUP);
+  return 0;
+}
+
+int fcx_keyboard_type_text(fcx_keyboard_t *keyboard, const char *text) {
+  struct fcx_keyboard *kb = (struct fcx_keyboard *)keyboard;
+  int text_len = strlen(text);
+  for (int i = 0; i < text_len; i++) {
+    struct fcx_keyboard_keycode_mapping *km =
+        _fcx_keyboard_keycode_mapping_from_char(keyboard, text[i]);
+    if (km == NULL) {
+      continue;
+    }
+    UInt32 modifiers_mask = 0;
+    if ((km->modifiers & shift) != 0) {
+      modifiers_mask |= NX_SHIFTMASK;
+    }
+    if ((km->modifiers & option) != 0) {
+      modifiers_mask |= NX_ALTERNATEMASK;
+    }
+    FCX_LOG_DEBUG("fcx_keyboard_type_text modifiers_mask: %08X, keycode: %d",
+                  modifiers_mask, km->keycode);
+    fcx_keyboard_set_modifiers(keyboard, modifiers_mask);
+    fcx_keyboard_type_keycode(keyboard, km->keycode);
+    fcx_keyboard_set_modifiers(keyboard, 0);
+  }
   return 0;
 }
 
