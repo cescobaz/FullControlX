4203e1266c3d5ae308df156baa3dc7dcead0ee34
Francesco Burelli <francesco.burelli@gmail.com>
2023-01-04 16:37:48 +0100

    MacOS app (build and) launch the Elixir server

--- xcode/FullControlX/AppDelegate.swift
+++ xcode/FullControlX/AppDelegate.swift
@@ -11,6 +11,7 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
     
     var statusItem: NSStatusItem?
+    var elixirProcess: Process?
     
     @objc func open(_ sender: Any) {
         if let url = URL(string: "https://github.com/cescobaz/FullControlX") {
@@ -39,15 +40,52 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         return statusItem
     }
     
+    func runElixir() throws -> Process {
+        guard let driverURL = Bundle.main.url(forResource: "fcxd", withExtension: "") else {
+            throw NSError(domain: "Resource not found", code: 1)
+        }
+        guard let exeURL = Bundle.main.url(forResource: "fullcontrol_x", withExtension: "", subdirectory: "fullcontrol_x/bin") else {
+            throw NSError(domain: "Resource not found", code: 2)
+        }
+        print("[INFO] driverURL \(driverURL), exeURL \(exeURL)")
+        
+        let databasePath = "\(NSHomeDirectory())/sqlite.db"
+        print("[INFO] databasePath \(databasePath)")
+        
+        let process = Process()
+        process.environment = [
+            "FCXD_PATH" : driverURL.path,
+            "DATABASE_PATH" :
+                databasePath,
+            "SECRET_KEY_BASE" : "tR+7w7T35Y9/NxvAKXGYrhD5dyBy/JwTATSWC/tMIesW/UphSNbDTF1bBtZ9kAyx",
+            "PORT" : "4000",
+            "PHX_HOST" : "localhost",
+            "PHX_SERVER" : "true"
+        ]
+        process.arguments = ["start"]
+        process.launchPath = exeURL.path
+        process.launch()
+        
+        return process
+    }
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         statusItem = createStatusBarItem()
+        do {
+            elixirProcess = try runElixir()
+        } catch {
+            print("[ERROR] \(error)")
+        }
     }
     
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
         if let statusItem = statusItem {
             statusItem.statusBar?.removeStatusItem(statusItem)
+        }
+        if let process = elixirProcess {
+            process.terminate()
         }
     }
     
