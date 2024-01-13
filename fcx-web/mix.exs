5e0432d9e0342cf82db9d8e3ab1db5067b82d3b3
Francesco Burelli <francesco.burelli@proton.me>
2024-01-14 00:36:19 +0100

    bump ecto

--- fcx-web/mix.exs
+++ fcx-web/mix.exs
@@ -34,7 +34,7 @@ defmodule FullControlX.MixProject do
     [
       {:phoenix, "~> 1.6.8"},
       {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.6"},
+      {:ecto_sql, "~> 3.11"},
       {:ecto_sqlite3, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
